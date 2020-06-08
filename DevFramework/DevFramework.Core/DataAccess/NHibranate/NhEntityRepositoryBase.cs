6ec77f920d0734f521ca0cfa0ee6e59395ab258e
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-06-08 15:42:09 +0300

    nhibarnate implement

---
+++ DevFramework/DevFramework.Core/DataAccess/NHibranate/NhEntityRepositoryBase.cs
@@ -0,0 +1,62 @@
+﻿using DevFramework.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using NHibernate.Linq;
+
+
+namespace DevFramework.Core.DataAccess.NHibranate
+{
+    public class NhEntityRepositoryBase<TEntity>:IEntityRepository<TEntity> where TEntity:class,IEntity,new()
+    {
+        private NHibarnateHelper _nhibarnate;
+        public NhEntityRepositoryBase(NHibarnateHelper nHibarnate)
+        {
+            _nhibarnate = nHibarnate;
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            using (var session= _nhibarnate.OpenSession())
+            {
+                session.Save(entity);
+                return entity;
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using(var session= _nhibarnate.OpenSession())
+            {
+                session.Delete(entity);
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (var session=_nhibarnate.OpenSession())
+            {
+                return session.Query<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using(var session = _nhibarnate.OpenSession())
+            {
+                return filter == null ? session.Query<TEntity>().ToList() : session.Query<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            using(var session = _nhibarnate.OpenSession())
+            {
+                session.Update(entity);
+                return entity;
+            }
+        }
+    }
+}
---
+++ DevFramework/DevFramework.Core/DataAccess/NHibranate/NhQueryableRepository.cs
@@ -0,0 +1,33 @@
+﻿using DevFramework.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevFramework.Core.DataAccess.NHibranate
+{
+    public class NhQueryableRepository<TEntity> : IQueryableRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        private NHibarnateHelper _nhibarnate;
+        private IQueryable<TEntity> _entities;
+        public NhQueryableRepository(NHibarnateHelper nHibarnate)
+        {
+            _nhibarnate = nHibarnate;
+            
+        }
+        public IQueryable<TEntity> Table
+        {
+            get { return this.Enitites; }
+        }
+
+        public virtual IQueryable<TEntity> Enitites {
+            get {
+                if (_entities == null)
+                {
+                    _entities = _nhibarnate.OpenSession().Query<TEntity>();
+                }
+                return _entities;
+             } 
+        }
+    }
+}

077eb00d52ecc32c7eba34b8eaed55f862d072f5
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-06-09 17:48:18 +0300

    nhibarnate dal

---
+++ DevFramework/DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
@@ -0,0 +1,17 @@
+﻿using DevFramework.Core.DataAccess.NHibranate;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate
+{
+   public  class NhProductDal : NhEntityRepositoryBase<Product> ,IProductDal
+    {
+        public NhProductDal(NHibarnateHelper nHibarnateHelper):base(nHibarnateHelper)
+        {
+
+        }
+    }
+}

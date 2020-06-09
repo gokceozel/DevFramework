018330a341b6a2f5730a7476127c6e3055bd4729
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-06-09 17:07:16 +0300

    sql server helper for nhibarnate

---
+++ DevFramework/DevFramework.Northwind.DataAccess/Concrete/NHibernate/Helper/SqlServerHelper.cs
@@ -0,0 +1,19 @@
+﻿
+using DevFramework.Core.DataAccess.NHibranate;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate.Helper
+{
+    public class SqlServerHelper : NHibarnateHelper
+    {
+        protected override ISessionFactory InitializeFactory()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}

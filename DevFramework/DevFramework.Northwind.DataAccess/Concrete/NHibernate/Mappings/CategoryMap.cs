78abbf64fc6b170a0db3ed3d86996ee1e9864a95
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-06-09 23:22:05 +0300

    category nhibarnate test finished.

---
+++ DevFramework/DevFramework.Northwind.DataAccess/Concrete/NHibernate/Mappings/CategoryMap.cs
@@ -0,0 +1,19 @@
+﻿using DevFramework.Northwind.Entities.Concree;
+using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate.Mappings
+{
+   public  class CategoryMap : ClassMap<Category>
+    {
+        public CategoryMap()
+        {
+            Table(@"Categories");
+            LazyLoad();
+            Id(x => x.CategoryID).Column("CategoryID");//primary key alanı
+            Map(x => x.CategoryName).Column("CategoryName");
+        }
+    }
+}
---
+++ DevFramework/DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhCategoryDal.cs
@@ -0,0 +1,17 @@
+﻿using DevFramework.Core.DataAccess.NHibranate;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate
+{
+    public class NhCategoryDal : NhEntityRepositoryBase<Category>,ICategoryDal
+    {
+        public NhCategoryDal(NHibarnateHelper nHibarnateHelper):base(nHibarnateHelper)
+        {
+
+        }
+    }
+}

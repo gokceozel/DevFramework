4a7fe38ca4ce797b57c18ec96c1726d201c5e633
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-06-10 16:51:28 +0300

    add validations in core

---
+++ DevFramework/DevFramework.Northwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using DevFramework.Northwind.Entities.Concree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFramework.Northwind.Business.Abstract
+{
+   public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        Product Add(Product product);
+    }
+}
---
+++ DevFramework/DevFramework.Northwind.Business/Concrete/Manager/ProductManager.cs
@@ -0,0 +1,32 @@
+﻿using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFramework.Northwind.Business.Concrete.Manager
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(x => x.ProductId == id);
+        }
+    }
+}
